807cb02cb3f739699e0a8c920f00ee6e6252ce15
godu <arthur.weber@viacesi.fr>
2014-08-21 22:20:00 +0200

    Fix test

--- test.js
+++ test.js
@@ -2,6 +2,8 @@ var NPMCM = require('./');
 
 if(NPMCM('package') !== 'package')
   throw new Error('package');
+if(NPMCM('npmrcpackage') !== 'npmrcpackage')
+  throw new Error('npmrc');
 if(NPMCM('npmrc') !== 'npmrc')
   throw new Error('npmrc');
 if(NPMCM('env') !== 'env')
